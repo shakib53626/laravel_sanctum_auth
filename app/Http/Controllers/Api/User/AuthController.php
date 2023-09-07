1c3b31af7ff730f4e5a7d89a3e2d8c77125a4d89
WebCodersbd403 <webcodersbd403@gmail.com>
2023-09-08 02:33:51 +0600

    Login response data send Successfully

---
+++ app/Http/Controllers/Api/User/AuthController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api\User;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\User\LoginRequest;
+use App\Http\Resources\User\AuthResource;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request){
+        $user = User::where('phone', $request->phone)->first();
+ 
+        if (! $user || ! Hash::check($request->password, $user->password)) {
+            throw ValidationException::withMessages([
+                'email' => ['The provided credentials are incorrect.'],
+            ]);
+        }
+ 
+    $token = $user->createToken('user-token')->plainTextToken;
+
+    return (new AuthResource($user))
+                ->additional(['meta' => [
+                    'token' => $token,
+                    'token_type' => 'Bearer',
+                ]]);
+
+    }
+}
